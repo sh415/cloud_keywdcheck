e0d37257b1c0e483bfc0d793c274bc18bdfbb104
DESKTOP-MO2U84O\user <apxkf9@gmail.com>
2023-10-17 21:56:46 +0900

    selenium 코드 적용

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import platform
 import time
 import requests
 from random import *
@@ -6,6 +7,13 @@ from flask_cors import CORS
 from flask_restful import Api
 from flasgger import Swagger, swag_from
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import WebDriverException
 
 app = Flask(__name__)
 api = Api(app)
@@ -33,7 +41,7 @@ def scrap_keywdcheck(keywd):
     try:
         url = f'https://m.search.naver.com/search.naver?sm=mtp_hty.top&where=m&query={keywd}'
         headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36 Edge/17.17134'
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36 Edg/118.0.2088.46'
         }
         response = requests.get(url, headers=headers)
         soup = BeautifulSoup(response.text, 'html.parser')
@@ -42,6 +50,23 @@ def scrap_keywdcheck(keywd):
         posts = []
         for post in svp.select('.api_txt_lines.total_tit'):
             href = post.get('href')
+            posts.append({'href': href})
+        
+        return posts
+
+    except Exception as e:
+        print('scrap_keywdcheck error', e)
+        return False
+
+def scrap_keywdcheck_v2(driver, keywd):
+    try:
+        driver.get(f'https://m.search.naver.com/search.naver?sm=mtp_hty.top&where=m&query={keywd}')
+        time.sleep(uniform(1.0, 2.0))
+
+        list = driver.find_element(By.CSS_SELECTOR, '._svp_list')
+        posts = []
+        for post in list.find_elements(By.CSS_SELECTOR, '.api_txt_lines.total_tit'):
+            href = post.get_attribute('href')
             posts.append({'href': href})
         
         return posts
@@ -111,17 +136,28 @@ def server_responses():
 })
 def keywdcheck():
     try:
+        options = webdriver.ChromeOptions()
+        options.add_argument('headless')
+        # options.add_experimental_option('excludeSwitches', ['enable-logging'])
+        operaitor = platform.system()
+        if (operaitor == 'Windows'):
+            service = Service(executable_path=r'/chromedriver-win64/chromedriver')
+            driver = webdriver.Chrome(service=service, options=options)
+        elif (operaitor == 'Linux'):
+            service = Service(executable_path=r'/chromedriver-linux64/chromedriver.exe')
+            driver = webdriver.Chrome(service=service, options=options)
+
         col = request.json.get('col')
         row = request.json.get('row')
 
         keywd = f'{col}+{row}'
-        posts1 = scrap_keywdcheck(keywd)
-
-        time.sleep(uniform(0.5, 1.0))
+        posts1 = scrap_keywdcheck_v2(driver, keywd)
 
         keywd = f'{col}{row}'
-        posts2 = scrap_keywdcheck(keywd)
+        posts2 = scrap_keywdcheck_v2(driver, keywd)
         marge_posts = posts1 + posts2
+
+        driver.quit()
 
         return { 'message': True, 'posts': marge_posts }, 200
 
