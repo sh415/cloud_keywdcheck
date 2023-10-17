06445ee6d1ad989247e09de9193fecf4dfed6a3a
DESKTOP-MO2U84O\user <apxkf9@gmail.com>
2023-10-17 22:10:40 +0900

    운영체제 및 모듈 버전 오류 수정

--- app.py
+++ app.py
@@ -144,8 +144,7 @@ def keywdcheck():
             service = Service(executable_path=r'/chromedriver-win64/chromedriver')
             driver = webdriver.Chrome(service=service, options=options)
         elif (operaitor == 'Linux'):
-            service = Service(executable_path=r'/chromedriver-linux64/chromedriver')
-            driver = webdriver.Chrome(service=service, options=options)
+            driver = webdriver.Chrome(r'/chromedriver-linux64/chromedriver', options=options)
 
         col = request.json.get('col')
         row = request.json.get('row')
