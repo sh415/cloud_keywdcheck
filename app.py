6d118e53a5336a025154bc5d900f14898033f76b
DESKTOP-MO2U84O\user <apxkf9@gmail.com>
2023-10-17 23:12:56 +0900

    드라이버 옵션 수정

--- app.py
+++ app.py
@@ -138,6 +138,10 @@ def keywdcheck():
     try:
         options = webdriver.ChromeOptions()
         options.add_argument('headless')
+        options.add_argument("no-sandbox")
+        options.add_argument('window-size=1920x1080')
+        options.add_argument("disable-gpu")
+        options.add_argument("lang=ko_KR")
         # options.add_experimental_option('excludeSwitches', ['enable-logging'])
         operaitor = platform.system()
         if (operaitor == 'Windows'):
